e6edab5d449a9f185e18c4f5ac43c7af7064d791
zhouyu131415 <40559829+zhouyu131415@users.noreply.github.com>
2018-06-26 23:40:27 +0800

    修改服务提供者

--- src/main/java/com/example/demo/FirstController.java
+++ src/main/java/com/example/demo/FirstController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class FirstController {
 	
 	@RequestMapping(value = "/person/{personId}",  method = RequestMethod.GET)
-	public String index() {
-		return "test";
+	public String index(@PathVariable("personId") Integer personId) {
+		return personId.toString();
 	}
 }
